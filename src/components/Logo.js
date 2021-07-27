80b480b898521447a23aa068be5c3f1cc612a98f
Zach Green <zachjamesgreen@gmail.com>
2021-07-27 17:00:12 -0600

    Create drin this logo component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,8 +5,8 @@ export default class Logo extends React.Component {
     return (
       <div className="border-4 border-black w-min m-8 p-2 font-playfairblack text-sm">
         <p className="w-max">drink this</p>
-        <p className="w-max">drink this</p>
-        <p className="w-max">drink this</p>
+        <p className="w-max opacity-70">drink this</p>
+        <p className="w-max opacity-50">drink this</p>
       </div>
     )
   }
