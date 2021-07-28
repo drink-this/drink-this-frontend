f51dff81a07805475734588851ec5b45b3334e51
Zach Green <zachjamesgreen@gmail.com>
2021-07-28 14:21:51 -0600

    rename imports

--- src/components/app.js
+++ src/components/app.js
@@ -7,10 +7,10 @@ import {
   Link
 } from "react-router-dom";
 import React from 'react';
-import Header from './Header.js';
+import Header from './header.js';
 import Landing from './landing.js';
 import Onboard from './onboard.js';
-import SearchResults from './SearchResults.js'
+import SearchResults from './search_results.js'
 
 import {
   GET_EXAMPLE_MESSAGE,
