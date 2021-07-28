45b90b60057babc312a5d2a2931a1f02b09701cd
Richard <rdesilvey@gmail.com>
2021-07-28 17:29:07 -0600

    add css

--- src/components/auth_panel.js
+++ src/components/auth_panel.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 export default class AuthPanel extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render () {
-        return (
-            <div className='min-w-full '>
-                <div className='text-center bg-gray-300 '>
-                    dkskdjfls
-                </div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  
+  render () {
+    return (
+      <div className='grid grid-cols-1'>
+        <div className='text-7xl p-10'>What should I drink tonight?</div>
+        <div className='px-10 pb-5'>Maybe it’s an old classic or maybe it’s something completely brand new. Tell us what you’re into and we’ll suggest something for you.</div>
+        <button className='border-2 border-black max-w-md'>Sign up with Google</button>
+        <button className='border-2 border-black max-w-md'>Sign In</button>
+      </div>
+    );
+  }
 }
