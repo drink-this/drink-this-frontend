c987f73ac36293e6e77afd2bd2a1642a9c90250a
Richard <rdesilvey@gmail.com>
2021-07-27 19:38:33 -0600

    Remove render manager

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import RenderManager from './core/RenderManager.js';
+import App from './components/App.js';
+import ReactDOM from 'react-dom';
 import './fonts/PlayfairDisplay-Black.ttf';
 import './index.css';
 
 require('./core/boot');
-require('./components/render');
 
-
-RenderManager.renderElements();
+let element = document.getElementById('app');
+if (element) {
+  ReactDOM.render(<App />, element);
+}
