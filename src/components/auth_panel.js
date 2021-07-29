7a35f8969e628116c694e099317ca84b48ed213d
Taija <taija.warbelow@gmail.com>
2021-07-28 18:26:10 -0700

    switch to button components

--- src/components/auth_panel.js
+++ src/components/auth_panel.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import SignInButton from './sign_in_button';
+import SignUpButton from './sign_up_button';
 
 export default class AuthPanel extends React.Component {
   constructor(props) {
@@ -7,11 +9,11 @@ export default class AuthPanel extends React.Component {
   
   render () {
     return (
-      <div className='grid grid-cols-1'>
-        <div className='text-7xl p-10'>What should I drink tonight?</div>
+      <div className='grid grid-cols-1 place-items-center'>
+        <div className='text-7xl p-10 text-center'>What should I drink tonight?</div>
         <div className='px-10 pb-5'>Maybe it’s an old classic or maybe it’s something completely brand new. Tell us what you’re into and we’ll suggest something for you.</div>
-        <button className='border-2 border-black max-w-md'>Sign up with Google</button>
-        <button className='border-2 border-black max-w-md'>Sign In</button>
+        <SignUpButton/>
+        <SignInButton/>
       </div>
     );
   }
