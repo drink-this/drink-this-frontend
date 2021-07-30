c17300810b2d725f2b5c19a5369f8611f4b16088
Richard <rdesilvey@gmail.com>
2021-07-30 08:44:05 -0600

    Add Google Sign in Component

--- src/components/sign_in_button.js
+++ src/components/sign_in_button.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import GoogleLogin from 'react-google-login';
 
 export default class SignInButton extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  _checkForToken = () => {
-    let token = localStorage.getItem('drink_this_user_id')
-
-    if (token === null) {
-      // do what sign up button does
-    } else {
-      // handle it as whatever
-    }
+  responseGoogle = (response) => {
+    console.log(response);// This is what is given to us by google
   }
 
   render () {
-    return <Link className='border-2 border-black max-w-md' to={{ pathname: "http://localhost:8080/auth/google_oauth2" }} target="_blank" onClick={this._checkForToken}>Sign In</Link>;
+    return <GoogleLogin
+    clientId=""
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />;
   }
 }
