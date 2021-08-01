866268e33c7ccb1179d9e5b3a95c4bf7d70739e4
Zach Green <zachjamesgreen@gmail.com>
2021-08-01 13:17:50 -0600

    add mock data to show page

--- src/components/show_page.js
+++ src/components/show_page.js
@@ -1,30 +1,45 @@
+import React from 'react'
 import Stars from './stars.js'
 import SocialLinks from './social_links.js'
+
 const ShowPage = () => {
+  let {name, thumbnail, recipe, instructions, rating} = {
+    "name":"Vodka Fizz",
+    "thumbnail":"https://www.thecocktaildb.com/images/media/drink/xwxyux1441254243.jpg",
+    "glass":"White wine glass",
+    "recipe":["2 oz Vodka","2 oz Half-and-half","2 oz Limeade","Ice","Nutmeg"],
+    "instructions":"Blend all ingredients, save nutmeg. Pour into large white wine glass and sprinkle nutmeg on top.",
+    "rating":4
+  }
+
+  instructions = instructions.split('. ')
+  console.log(instructions);
+  
   return (
     <section className="flex justify-evenly">
       <div>
         <p className="font-playfair italic text-3xl pb-3">You should have a...</p>
-        <h1 className="font-playfair font-black text-6xl pb-2">Cocktial Name</h1>
+        <h1 className="font-playfair font-black text-6xl pb-2">{ name }</h1>
         <p className="font-montserrat italic">Rate me to get better recommendations</p>
         <div>
-          <Stars cname="flex pb-8"/>
+          <Stars cname="flex pb-8" value={rating} />
         </div>
-        <div className="grid grid-cols-2">
+        <div className="flex space-x-10">
           <div>
             <p className="font-montserrat font-bold uppercase">Ingredients</p>
             <ul>
-              {Array.from({ length: 4}, (v,i) => {
-                return <li key={i}>Ingredient {i}</li>
+              {recipe.map((ingredient, i) => {
+                return <li key={i}>{ingredient}</li>
               })}
             </ul>
           </div>
-          <div>
+          <div className="max-w-xs">
             <p className="font-montserrat font-bold uppercase">Directions</p>
             <ol className="list-decimal ml-4">
-              {Array.from({ length: 4}, (v,i) => {
-                return <li key={i}>Step {i}</li>
+              {instructions.map((step, i) => {
+                return <li key={i}>{step}</li>
               })}
+
             </ol>
           </div>
         </div>
@@ -32,7 +47,7 @@ const ShowPage = () => {
       <div className="flex justify-around">
 
         <div className="font-montserrat font-semibold">
-          <img src="https://via.placeholder.com/450" alt="" />
+          <div className="cocktail_image w-96 h-96 bg-cover" style={{backgroundImage: `url("${thumbnail}")`}}></div>
           <button className="uppercase border-2 border-black px-4 py-2 mt-4 w-full hover:bg-black hover:text-white transition">Find a different drink</button>
         </div>
 
