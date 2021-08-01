a5e318cc3ecc281952583f59fe82ec941dc2ee64
Taija <taija.warbelow@gmail.com>
2021-07-31 21:39:22 -0700

    change text color

--- src/components/auth_panel.js
+++ src/components/auth_panel.js
@@ -9,8 +9,8 @@ export default class AuthPanel extends React.Component {
   render () {
     return (
       <div className='grid grid-cols-1 gap-2 place-items-center'>
-        <div className='text-7xl p-10 text-center'>What should I drink tonight?</div>
-        <div className='px-10 pb-5'>Maybe it’s an old classic or maybe it’s something completely brand new. Tell us what you’re into and we’ll suggest something for you.</div>
+        <div className='text-7xl p-10 text-center text-white'>What should I drink tonight?</div>
+        <div className='px-10 pb-5 text-white'>Maybe it’s an old classic or maybe it’s something completely brand new. Tell us what you’re into and we’ll suggest something for you.</div>
         <SignInButton />
       </div>
     );
