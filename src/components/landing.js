2f543b41ea926be87f09d14dd2f0b53dbd5a570f
Taija <taija.warbelow@gmail.com>
2021-07-31 22:02:33 -0700

    update logo and background image

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AuthPanel from './auth_panel';
+import WhiteLogo from './white_logo';
 
 export default class Landing extends React.Component {
   render() {
     return (
       <div className='bg-hero-pattern bg-cover'>
+        <WhiteLogo />
         <div className='grid grid-cols-11 w-screen h-screen place-items-center'>
           <div className='col-span-7 '></div>
           <div className='col-span-4'>
