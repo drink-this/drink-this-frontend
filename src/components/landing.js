1f0595b0b5e621a575e08ce4cde6f4d71ded3baa
Taija <taija.warbelow@gmail.com>
2021-07-31 21:40:00 -0700

    refactor for background image, update columns

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import LandingImage from './landing_image';
 import AuthPanel from './auth_panel';
 
 export default class Landing extends React.Component {
   render() {
     return (
-      <div className='grid grid-cols-2 w-screen h-screen place-items-center'>
-        <LandingImage src='/images/drew_beamer_on_unsplash_friends_cheers.png'/>
-        <AuthPanel/>
+      <div className='bg-hero-pattern bg-cover'>
+        <div className='grid grid-cols-11 w-screen h-screen place-items-center'>
+          <div className='col-span-7 '></div>
+          <div className='col-span-4'>
+           <AuthPanel/>
+          </div>
+        </div>
       </div>
     );
   }
