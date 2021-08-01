7b0bca7e604e578493d1a0be683fb68216f81aec
Taija <taija.warbelow@gmail.com>
2021-08-01 14:31:06 -0700

    axios call to backend

--- src/actions/auth_actions/confirm_login_action.js
+++ src/actions/auth_actions/confirm_login_action.js
@@ -4,9 +4,20 @@ import Router, { checkStatus, handleError } from '../../core/router.js';
 import { CONFIRM_LOG_IN } from '../../constants.js';
 import authStore from "../../stores/auth_store";
 import Cookies from 'js-cookie';
+import { Callbacks } from "jquery";
+import axios from "axios";
 
 Actions.register(CONFIRM_LOG_IN, payload => {
   Cookies.set('authToken', payload.authToken, { expires: 1 });
+  axios.get('/authorization', {
+    params: {
+      auth_token: payload.authToken,
+      email: payload.email
+    }
+  })
+  .then(checkStatus)
+  .then(response => {
+  console.log(response);
   // Axios(Router.request('GET', CONFIRM_LOG_IN, {
   //   params: {
   //     auth_token: payload.authToken,
@@ -20,5 +31,6 @@ Actions.register(CONFIRM_LOG_IN, payload => {
     authStore.setIsUserNew(true); // set for now, remove later
     window.location = "/onboard"
     Actions.finish(payload);
-  // });
+  });
 });
+
