d8abf79f23bb3d2e69be058db89861bbbb2f236d
Richard <rdesilvey@gmail.com>
2021-08-02 16:32:02 -0600

    Handle error

--- src/actions/auth_actions/confirm_login_action.js
+++ src/actions/auth_actions/confirm_login_action.js
@@ -1,6 +1,6 @@
 import Actions from "../../core/app_actions";
 import Axios from 'axios';
-import Router, { checkStatus } from '../../core/router.js';
+import Router, { checkStatus, handleError } from '../../core/router.js';
 import { CONFIRM_LOG_IN } from '../../constants.js';
 import authStore from "../../stores/auth_store";
 
@@ -15,7 +15,7 @@ Actions.register(CONFIRM_LOG_IN, payload => {
     authStore.setAuthed(response.data.authToken);
     authStore.setIsUserNew(response.data.is_new);
     Actions.finish(payload)
-  });
+  }).catch(handleError);
     // authStore.setAuthed(payload.authToken);
     // authStore.setIsUserNew(true);
     // Actions.finish(payload);
