9ae3634444abe32813f191fe1cfefe33a3f8b523
Zach Green <zachjamesgreen@gmail.com>
2021-08-02 21:43:27 -0600

    tweak styles to get rid of scroll bars

--- src/components/landing.js
+++ src/components/landing.js
@@ -5,11 +5,9 @@ import WhiteLogo from './white_logo';
 export default class Landing extends React.Component {
   render() {
     return (
-      <div className='bg-hero-pattern bg-cover w-screen h-screen'>
-        <div className="flex">
-          <WhiteLogo />
-        </div>
-        <div className='grid grid-cols-11  place-items-center'>
+      <div className='bg-hero-pattern bg-cover'>
+        <WhiteLogo />
+        <div className='grid grid-cols-11 w-screen h-screen place-items-center'>
           <div className='col-span-7 '></div>
           <div className='col-span-4'>
            <AuthPanel/>
