3f6132cf51636acd34e237d5d3d1424cc16fd401
Zach Green <zachjamesgreen@gmail.com>
2021-08-04 16:22:15 -0600

    WIP fix test

--- src/test/search_results.test.js
+++ src/test/search_results.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import SearchResults from '../components/search_results.js';
 import { render, unmountComponentAtNode  } from 'react-dom';
-import { act } from "react-dom/test-utils";
 import { Router, Route } from 'react-router'
 import { createMemoryHistory } from 'history'
+// import { waitFor } from '@testing-library/react';
 const history = createMemoryHistory({ initialEntries: ['/search?q=drink'] })
 
 let container = null;
