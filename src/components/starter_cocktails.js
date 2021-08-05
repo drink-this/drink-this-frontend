84c2425665b2703559c1a6185ea01fe0af3d48d2
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-05 16:11:58 -0600

    Remove unneeded code

--- src/components/starter_cocktails.js
+++ src/components/starter_cocktails.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Stars from './stars.js';
-import RecommendationButton from  './recommendation_button.js';
 
 export default class StarterCocktails extends React.Component {
   disabledClasses = "flex justify-center uppercase border-2 text-gray-400 border-gray-400 py-2 px-20 m-8 w-1/3 hover:text-gray-500 hover:bg-white transition"
@@ -35,9 +34,6 @@ export default class StarterCocktails extends React.Component {
       <div>
         <div className="flex justify-center space-x-8">
           {data.map((cocktail,index) => {
-            // if (cocktail.attributes.rating > 0 && !this.state.clicked.includes(index)) {
-            //   this.setState({clicked: [...this.state.clicked, index]})
-            // } 
             return (
               <div className="text-center" id={index} key={cocktail.name}>
                 <img className='h-60 w-60' src={cocktail.attributes.thumbnail} alt="Cocktail" />
