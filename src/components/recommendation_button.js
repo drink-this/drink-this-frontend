d887abc226cbc20750ca2b3c35fd6c60b4ba19bd
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-04 20:59:46 -0600

    Add RecommendationButton to support StarterCocktails

---
+++ src/components/recommendation_button.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default class RecommendationButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: this.props.clicked
+    }
+  }
+
+  parseParams = (props) => {
+    const { location: { clicked } } = props;
+    let parsed = queryString.parse(search);
+    return parsed.q;
+  }
+
+  render() {
+    if (clicked == 5) {
+      return (
+      <div className="flex justify-center">
+        <a href="/recommendation" 
+        className="flex justify-center uppercase border-2 border-black py-2 px-20 m-8 w-1/3 hover:text-white hover:bg-black transition"
+        >Find me a drink!</a>
+      </div> 
+      )
+    } else {
+      return (
+        <div className="flex justify-center">
+          <a href="/recommendation" 
+          className="flex justify-center uppercase border-2 border-black py-2 px-20 m-8 w-1/3 hover:text-white hover:bg-pink transition"
+          >Find me a drink!</a>
+        </div> 
+        )
+    }  
+  }
+}
