f376b1e1ff33987d3c4504b019966a52efd564fe
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-04 20:59:00 -0600

    Refactor onboard to use StarterCocktails component

--- src/components/onboard.js
+++ src/components/onboard.js
@@ -1,51 +1,42 @@
 import React from 'react';
-import Stars from './stars.js'
+import StarterCocktails from './starter_cocktails.js';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 export default class Onboard extends React.Component {
-  render() {
-    let data = [
-      {
-        "name": "Cocktail 1",
-        "image": 'http://via.placeholder.com/200/100'
-      },
-      {
-        "name": "Cocktail 2",
-        "image": 'http://via.placeholder.com/200/200'
-      },
-      {
-        "name": "Cocktail 3",
-        "image": 'http://via.placeholder.com/200/300'
-      },
-      {
-        "name": "Cocktail 4",
-        "image": 'http://via.placeholder.com/200/400'
-      },
-      {
-        "name": "Cocktail 5",
-        "image": 'http://via.placeholder.com/200/500'
-      },
-    ]
-    return(
+  source = axios.CancelToken.source()
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      isLoaded: false
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`${process.env.REACT_APP_SERVER_URL}/api/v1/dashboard`, {cancelToken: this.source.token, params:{auth_token: Cookies.get('authToken')}})
+      .then((res) => {
+        this.setState({data: res.data.data, isLoaded: true})
+      })
+      .catch((err) => {
+        this.source.cancel('unmounting')
+      })
+  }
+
+  componentWillUnmount() {
+    this.source.cancel('unmounting')
+  }
+
+  render () {
+    if (!this.state.isLoaded) {
+      return <div className="font-playfair font-normal text-3xl text-center mx-56">Loading...</div>
+    } else {
+      return (
       <div className="font-playfair">
         <h1 className="text-center mb-12 text-3xl">To give you the best recommendations, please rate these cocktails</h1>
-        <div className="flex justify-center space-x-8">
-        {data.map((cocktail) => {
-          return (
-            <div className="text-center" key={cocktail.name}>
-              <img src={cocktail.image} alt="Cocktail" />
-              <p className="mt-4">Cocktail Name</p>
-              <Stars cname="stars flex mt-4 justify-center" name={cocktail.name}/>
-            </div>
-          )
-        })}
-        </div>
-        <div className="flex justify-center">
-          <a href="/recommendation" 
-          className="flex justify-center uppercase border-2 border-black py-2 px-20 m-8 w-1/3 hover:text-white hover:bg-black transition"
-          >Find me a drink!</a>
-        </div>
+        <StarterCocktails data={this.state.cocktails}/>
       </div>
-    )
+      )
+    }
   }
 }
-
