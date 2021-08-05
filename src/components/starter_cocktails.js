9fed1f81b6969f420ff26561abcb8afe739dd7e5
Zach Green <zachjamesgreen@gmail.com>
2021-08-05 14:34:01 -0600

    WIP onboard page things

--- src/components/starter_cocktails.js
+++ src/components/starter_cocktails.js
@@ -3,39 +3,41 @@ import Stars from './stars.js';
 import RecommendationButton from  './recommendation_button.js';
 
 export default class StarterCocktails extends React.Component {
+  disabledClasses = "flex justify-center uppercase border-2 text-gray-400 border-gray-400 py-2 px-20 m-8 w-1/3 hover:text-gray-500 hover:bg-white transition"
+  enabledClasses = "flex justify-center uppercase border-2 text-white bg-black border-black py-2 px-20 m-8 w-1/3 hover:text-black hover:bg-white transition"
   constructor(props) {
     super(props);
     this.state = {
       data: this.props.data,
-      clicked: []
+      clicked: [],
+      disabled: true
     }
   }
-
-  // setClicked (cocktail_data) {
-  //   cocktail_data.map((cocktail, index) => {
-  //     if (cocktail.attributes.rating > 0) {
-  //       index
-  //     }
-  //   })
-  // }
 
   handleClick = (e) => {
     var index = e.target.parentNode.parentNode.getAttribute('id')
     if (!this.state.clicked.includes(index)) {
       this.setState({clicked: [...this.state.clicked, index]})
+      this.checkDisabled([...this.state.clicked, index])
+    }
+  }
+
+  checkDisabled = (ary) => {
+    if (ary.length >= 5) {
+      this.setState({disabled: false})
     }
   }
 
   render() {
-    let data = this.state.data;
+    let {data, clicked, disabled} = this.state;
 
     return (      
       <div>
         <div className="flex justify-center space-x-8">
           {data.map((cocktail,index) => {
-            if (cocktail.attributes.rating > 0 && !this.state.clicked.includes(index)) {
-              this.setState({clicked: [...this.state.clicked, index]})
-            } 
+            // if (cocktail.attributes.rating > 0 && !this.state.clicked.includes(index)) {
+            //   this.setState({clicked: [...this.state.clicked, index]})
+            // } 
             return (
               <div className="text-center" id={index} key={cocktail.attributes.name}>
                 <img src={cocktail.attributes.thumbnail} alt="Cocktail" />
@@ -48,7 +50,13 @@ export default class StarterCocktails extends React.Component {
           }
           )}
         </div>
-        <RecommendationButton clicked={this.state.clicked}/>
+
+        <div className="flex justify-center">
+          <button disabled={disabled}
+          className={disabled ? this.disabledClasses : this.enabledClasses}
+          >Find me a drink!</button>
+        </div> 
+
       </div>  
     ) 
   }
