b4f6e7d9c2010234eb3afdfdd6027cddff4a02f8
Richard <rdesilvey@gmail.com>
2021-08-17 10:34:47 -0600

    Check for new user
    Navigate to dashboard

--- src/components/onboard.js
+++ src/components/onboard.js
@@ -1,52 +1,51 @@
 import React from 'react';
 import StarterCocktails from './starter_cocktails.js';
-import axios from 'axios';
 import { GET_ONBOARD_DRINKS } from '../constants.js';
 import AppDispatcher from '../core/dispatcher.js';
 import onboardStore from '../stores/onboard_store.js';
+import googleAuthStore from '../stores/google_auth_store.js';
+import { useEffect, useState } from 'react';
+import cocktailStore from '../stores/cocktail_store.js';
+import { useHistory } from 'react-router-dom';
 
-export default class Onboard extends React.Component {
-  source = axios.CancelToken.source()
-  constructor(props) {
-    super(props);
-    this.state = {
-      cocktails: [],
-      isLoaded: false
-    }
+export default function Onboard(props) {
+  const [cocktails, setCocktails] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  let history = useHistory();
+
+  const setcocktails = () => {
+    setCocktails(cocktailStore.cocktails);
+    setIsLoaded(true);
   }
 
-  setcocktails = () => {
-    let cocktails = onboardStore.cocktails;
-    this.setState({cocktails: cocktails, isLoaded: true});
-  }
-
-  componentDidMount() {
-    onboardStore.on(GET_ONBOARD_DRINKS, this.setcocktails);
-    AppDispatcher.dispatch({
-      action: GET_ONBOARD_DRINKS,
-      cancelTokenSource: this.source,
-      emitOn: [{
-        store: onboardStore,
-        ids: [GET_ONBOARD_DRINKS]
-      }]
-    });
-  }
-
-  componentWillUnmount() {
-    this.source.cancel('unmounting')
-    onboardStore.removeListener(GET_ONBOARD_DRINKS, this.setcocktails);
-  }
-
-  render () {
-    if (!this.state.isLoaded) {
-      return <div className="font-playfair font-normal text-3xl text-center mx-56">Loading...</div>
+  useEffect(() => {
+    onboardStore.on(GET_ONBOARD_DRINKS, setcocktails);
+    if (googleAuthStore.isUserNew() === 'true') {
+      AppDispatcher.dispatch({
+        action: GET_ONBOARD_DRINKS,
+        emitOn: [{
+          store: onboardStore,
+          ids: [GET_ONBOARD_DRINKS]
+        }]
+      });
     } else {
-      return (
-      <div className="font-playfair">
-        <h1 className="text-center mb-12 text-3xl">To give you the best recommendations, please rate these cocktails</h1>
-        <StarterCocktails data={this.state.cocktails}/>
-      </div>
-      )
+      history.replace('/dashboard');
     }
+
+    return function cleanup() {
+      onboardStore.removeListener(GET_ONBOARD_DRINKS, setcocktails);
+    };
+  });
+
+  if (!isLoaded) {
+    return <div className="font-playfair font-normal text-3xl text-center mx-56">Loading...</div>
+  } else {
+    return (
+    <div className="font-playfair">
+      <h1 className="text-center mb-12 text-3xl">To give you the best recommendations, please rate these cocktails</h1>
+      <StarterCocktails data={cocktails}/>
+    </div>
+    )
   }
 }
