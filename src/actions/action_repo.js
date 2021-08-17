528bfb18632731dcf6dbce3082e6f0017e6c91f3
Richard <rdesilvey@gmail.com>
2021-08-17 12:04:28 -0600

    Remove example store/action

--- src/actions/action_repo.js
+++ src/actions/action_repo.js
@@ -1,5 +1,4 @@
 // Require All of your Application action files Here to get them registered
-import './example_store_actions/get_example_message.js';
 import './auth_actions/confirm_login_action.js';
 import './auth_actions/logout_action.js';
 import './get_cocktail_actions/get_cocktails_action.js';
