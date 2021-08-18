8f059a0ee772dda8f7dd6630feb8c9549eaecbca
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-18 14:03:04 -0600

    Change age prompt background color and delete drinking age file

--- src/components/modal.js
+++ src/components/modal.js
@@ -17,7 +17,7 @@ export default class Modal extends React.Component {
   render() {
     return (
       <section className={`${this.props.cname} bg-black bg-opacity-50 absolute top-0 left-0 h-screen w-screen`}>
-        <div className="bg-age-pattern border-4 border-blue-300 border-opacity-75 fixed top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 filter drop-shadow-c p-6">
+        <div className="bg-white border-4 border-grey-300 border-opacity-75 fixed top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 filter drop-shadow-c p-6">
           <div className="font-montserrat font-bold uppercase m-2 text-3xl text-center">To enter, you must be at least 21 years old.</div><br/>
           <div className="flex space-x-4 m-2">
             <button
