3f8f148a2054f6097cd7a28a5a1bd2dca33c9008
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-18 09:28:21 -0600

    Modify landing for age prompt

--- src/components/landing.js
+++ src/components/landing.js
@@ -10,7 +10,6 @@ export default class Landing extends React.Component {
     super(props);
     this.state = {
       userConfirmed: authStore.isUserOfAge()
-      // userConfirmed: true
     };
   }
 
@@ -20,44 +19,29 @@ export default class Landing extends React.Component {
       userConfirmed: true
     });
   }
-  
+
   underAge = () => {
-    alert("Sorry too young!");
+    window.open("https://www.cdc.gov/alcohol/fact-sheets/underage-drinking.htm");
   }
 
   render() {
-    // if (this.state.userConfirmed) {
-      return (
-        <div className='bg-hero-pattern bg-cover h-screen'>
-          <div className="flex">
-            <WhiteLogo />
-          </div>
-          <div className='grid grid-cols-11 place-items-center'>
-            <div className='col-span-7 '></div>
-            <div className='col-span-4'>
-             <AuthPanel/>
-            </div>
-          </div>
-          <Modal
-          yes={this.userConfirm}
-          no={this.underAge}
-          cname={this.state.userConfirmed ? 'hidden' : 'block'}
-          />
+    return (
+      <div className='bg-hero-pattern bg-cover h-screen'>
+        <div className="flex">
+          <WhiteLogo />
         </div>
-      );
-    // } else {
-      // return (
-        // <section className="flex justify-evenly">
-        //   <div className='age-consent'>
-        //     <p className="font-montserrat font-bold uppercase">Are you 21 years or older?</p><br/>
-        //     <button onClick = {this.userConfirm} className="flex flex-col w-60 font-montserrat font-semibold border-2 border-black px-8 py-2 text-xl disabled:opacity-50">
-        //     Yes
-        //     </button><br/>
-        //     <button onClick={this.underAge}  className="flex flex-col w-60 font-montserrat font-semibold border-2 border-black px-8 py-2 text-xl disabled:opacity-50">
-        //     No
-        //     </button>
-        //   </div>
-        // </section>
-      // );
+        <div className='grid grid-cols-11 place-items-center'>
+          <div className='col-span-7 '></div>
+          <div className='col-span-4'>
+           <AuthPanel/>
+          </div>
+        </div>
+        <Modal
+        yes={this.userConfirm}
+        no={this.underAge}
+        cname={this.state.userConfirmed ? 'hidden' : 'block'}
+        />
+      </div>
+    );
   }
 }
