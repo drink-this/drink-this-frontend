b6b7a1c8cd98d0e55d02936aed9699484c3fbe13
Zach Green <zachjamesgreen@gmail.com>
2021-08-18 12:14:55 -0600

    styles for homepage

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,8 +2,70 @@ import { Link } from "react-router-dom"
 
 const Dashboard = () => {
   return (
-    <div className="h-full flex justify-center mt-52">
-      <Link className={"text-7xl border-2 border-black px-8 py-6 hover:bg-black hover:text-white transition"} to={"/recommendation"}>Get a Cocktail Recommendation</Link>
+    <div className="pb-20 text-center">
+      <div className="my-8">
+        <Link className={"border-2 border-black px-4 py-3 hover:bg-black hover:text-white transition"} to={"/recommendation"}>Get a Cocktail Recommendation</Link>
+      </div>
+
+      <div id="discover" className="text-center font-playfair">
+        <p className="text-3xl mb-4">
+          Your top cocktails
+          
+        </p>
+        <div className="flex justify-between mx-40 space-x-10 mb-10">
+          {Array.from({length: 5}).map((item, index) => {
+            return (
+              <Link to={'/'} className={'space-y-4'}>
+                <img src="https://via.placeholder.com/400" alt="" />
+                <p>Cocktail Name</p>
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+
+      <div id="discover" className="text-center font-playfair">
+        <p className="text-3xl mb-4">Discover something new</p>
+        <div className="flex justify-between mx-40 space-x-10 mb-10">
+          {Array.from({length: 5}).map((item, index) => {
+            return (
+              <Link to={'/'} className={'space-y-4'}>
+                <img src="https://via.placeholder.com/400" alt="" />
+                <p>Cocktail Name</p>
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+
+      <div id="glass" className="text-center font-playfair">
+        <p className="text-3xl mb-4">Cocktails in a <span className="italic">'coup'</span> glass</p>
+        <div className="flex justify-between mx-40 space-x-10 mb-10">
+          {Array.from({length: 5}).map((item, index) => {
+            return (
+              <Link to={'/'} className={'space-y-4'}>
+                <img src="https://via.placeholder.com/400" alt="" />
+                <p>Cocktail Name</p>
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+
+      <div id="alcohol" className="text-center font-playfair">
+        <p className="text-3xl mb-4">Cocktails with <span className="italic">'rum'</span></p>
+        <div className="flex justify-between mx-40 space-x-10 mb-10">
+          {Array.from({length: 5}).map((item, index) => {
+            return (
+              <Link to={'/'} className={'space-y-4'}>
+                <img src="https://via.placeholder.com/400" alt="" />
+                <p>Cocktail Name</p>
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+
     </div>
   );
 }
