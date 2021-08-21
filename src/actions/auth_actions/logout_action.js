4cbd6a09649a56d4b7b7ab0711835b7e221340f6
Richard <rdesilvey@gmail.com>
2021-08-20 18:18:48 -0600

    Use constant

--- src/actions/auth_actions/logout_action.js
+++ src/actions/auth_actions/logout_action.js
@@ -1,9 +1,9 @@
 import Actions from "../../core/app_actions";
-import { GOOGLE_TOKEN_NAME, LOGOUT } from '../../constants.js';
+import { AUTH_SERVICE, GOOGLE_TOKEN_NAME, LOGOUT } from '../../constants.js';
 import Cookies from "js-cookie";
 
 Actions.register(LOGOUT, payload => {
   Cookies.remove(GOOGLE_TOKEN_NAME);
-  Cookies.remove('service');
+  Cookies.remove(AUTH_SERVICE);
   Actions.finish(payload)
 });
